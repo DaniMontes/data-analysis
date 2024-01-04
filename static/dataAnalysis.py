7675f144028425b2340c4ffe60bdd787bcb8152b
DaniMontes <51269595+DaniMontes@users.noreply.github.com>
2024-01-04 15:37:24 +0000

    json iteration ready

--- static/dataAnalysis.py
+++ static/dataAnalysis.py
@@ -1,5 +1,38 @@
+import os
 import numpy as np
+import json
 
-my_array = np.array([[1,2,3],[4,5,6]])
+# get the current working directory
+current_working_directory = os.getcwd()
 
-print(my_array)
+with open('/workspaces/data-analysis/static/taylor_swift_spotify.json', "r") as json_file:
+    json_data = json.load(json_file)
+
+# Dictionary
+for artist_key, artist_value in json_data.items():
+    
+    # List
+    if artist_key == 'albums':    
+        print('Amount of albums: ', len(artist_value))   
+
+        for album in artist_value:                
+            for album_key, album_value in album.items():
+
+                if album_key == 'tracks':    
+                    print('Amount of tracks: ', len(album_value))
+
+                    for track in album_value:                
+                        for track_key, track_value in track.items():
+
+                            if track_key == 'audio_features': 
+                                print(track_value['energy'])
+
+
+                if album_key == 'album_total_tracks':
+                    print('album_total_tracks: ', album_value)
+
+    else:
+        print(artist_value)
+    
+
+    
